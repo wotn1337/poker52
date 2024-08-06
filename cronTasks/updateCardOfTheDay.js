2b1ad2ae0f79c563f9abddaafcb92daecff2eb92
Артем Белов <theflash.0320@gmail.com>
2024-08-06 20:40:47 +0500

    add updateCardOfTheDay.js

---
+++ cronTasks/updateCardOfTheDay.js
@@ -0,0 +1,37 @@
+const { MongoClient } = require("mongodb");
+const { mongoURI, collectionName } = require("./config");
+
+async function updateCardOfTheDay() {
+  const client = new MongoClient(mongoURI);
+  try {
+    await client.connect();
+    const db = client.db();
+    const collection = db.collection(collectionName);
+
+    // Сброс текущего cardOfTheDay
+    await collection.updateOne(
+      { cardOfTheDay: true },
+      { $set: { cardOfTheDay: false } }
+    );
+
+    // Получение случайной записи
+    const randomCard = await collection
+      .aggregate([{ $sample: { size: 1 } }])
+      .toArray();
+
+    if (randomCard.length > 0) {
+      // Установка cardOfTheDay на случайную запись
+      await collection.updateOne(
+        { _id: randomCard[0]._id },
+        { $set: { cardOfTheDay: true } }
+      );
+    }
+    console.log("Карта дня успешно обновлена");
+  } catch (err) {
+    console.error("Ошибка при обновлении cardOfTheDay:", err);
+  } finally {
+    await client.close();
+  }
+}
+
+updateCardOfTheDay();
