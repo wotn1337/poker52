a712afd9f5f2687c1060ac06384ac231d97bd286
Артем Белов <theflash.0320@gmail.com>
2024-08-11 22:22:46 +0500

    log mongodbURI

--- cronTasks/updateCardOfTheDay.js
+++ cronTasks/updateCardOfTheDay.js
@@ -1,7 +1,8 @@
-require("./config");
+const { mongoURI } = require("./config");
 const { updateInstanceOfTheDay } = require("./updateInstanceOfTheDay");
 
 async function updateCardOfTheDay() {
+  console.debug(mongoURI);
   const newCard = await updateInstanceOfTheDay("cards", "Карта");
 
   console.log(`Новая карта дня - ${newCard.value} ${newCard.kind}`);
